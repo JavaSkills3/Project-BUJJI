337ff8c0f272744ed1228f10b8762884c119347e
Sukumar755 <sukumar755@gmail.com>
2026-02-21 20:23:00 -0600

    Project initialization

---
+++ src/main/java/com/career/demo/BujjiApplication.java
@@ -0,0 +1,13 @@
+package com.career.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BujjiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BujjiApplication.class, args);
+	}
+
+}
