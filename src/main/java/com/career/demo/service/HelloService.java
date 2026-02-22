af4dcdb24435596f18664a47126d9bf977c97ce6
Sukumar755 <sukumar755@gmail.com>
2026-02-21 20:52:34 -0600

    Added layers and added service class and controller class

---
+++ src/main/java/com/career/demo/service/HelloService.java
@@ -0,0 +1,13 @@
+package com.career.demo.service;
+
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class HelloService {
+    public String sayHello(){
+        return "Hello Man!";
+    }
+
+
+}
